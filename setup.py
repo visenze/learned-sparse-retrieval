630032f0cd86fc091e15d456d5c6505866fa44d5
ShereefBa <shereefbankole@rezolve.com>
2026-01-09 18:24:57 +0000

    Add packaging configuration for pip/uv  installation

--- setup.py
+++ setup.py
@@ -1,39 +1,59 @@
-from setuptools import setup, find_packages
 import os
-import sys
 
+from setuptools import find_packages, setup
 
-long_description = "Learned Sparse Retrieval: https://github.com/visenze/learned-sparse-retrieval/"
+long_description = (
+    "Learned Sparse Retrieval: https://github.com/visenze/learned-sparse-retrieval/"
+)
 
 # Package meta-data.
 NAME = "lsr"
-DESCRIPTION = "Learned Sparse Retrieval"
-LONG_DESCRIPTION = long_description
+DESCRIPTION = "Learned Sparse Retrieval - A package for training and inference with learned sparse retrieval models"
 URL = "https://github.com/visenze/learned-sparse-retrieval/"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "0.0.1"
+VERSION = "1.0.0"
 
 with open("requirements.txt") as f:
-    REQUIRED = [line.rstrip('\n') for line in f]
-LINKS = []
-EXTRAS = {}
+    REQUIRED = [
+        line.rstrip("\n") for line in f if line.strip() and not line.startswith("#")
+    ]
+
+EXTRAS = {
+    "dev": [
+        "pytest>=7.0.0",
+        "pytest-cov>=3.0.0",
+        "black>=22.0.0",
+        "flake8>=4.0.0",
+        "isort>=5.10.0",
+    ]
+}
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(),
+    project_urls={
+        "Homepage": URL,
+        "Repository": URL,
+        "Documentation": URL,
+    },
+    packages=find_packages(exclude=["tests", "tests.*", "docs", "images"]),
     install_requires=REQUIRED,
-    dependency_links=LINKS,
     extras_require=EXTRAS,
     include_package_data=True,
-    entry_points={
-        "console_scripts": [
-        ]
+    package_data={
+        "lsr": ["configs/**/*.yaml", "**/*.yaml"],
     },
-    classifiers=["Programming Language :: Python :: 3.9", ],
+    entry_points={"console_scripts": []},
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="sparse retrieval information-retrieval neural-search transformers",
 )
